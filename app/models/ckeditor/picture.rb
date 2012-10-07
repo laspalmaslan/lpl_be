35adf43ef19b831f93e206e95b217403aee5aa6e
groteck <groteck@gmail.com>
2012-10-07 22:03:49 +0100

    solved sintex problems

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -7,7 +7,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :bucket            => ENV['S3_BUCKET_NAME'],
                     :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
-                    :path => "images/:id/:style_:basename.:extension",
+                    :path => "images/:id/:style_:basename.:extension"
                     
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
