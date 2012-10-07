60647bb2ce6211f1100aba9db34cc7f4a26482de
groteck <groteck@gmail.com>
2012-10-07 22:14:30 +0100

    WIP s3 config to files

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,16 +1,17 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
                     :storage => :s3,
-                      :s3_credentials => {
-                      :bucket            => ENV['S3_BUCKET_NAME'],
-                      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                    }
-  
+                    :s3_credentials => {
+                    :s3_host_name  =>"s3-eu-west-1.amazonaws.com",
+                    :bucket            => ENV['S3_BUCKET_NAME'],
+                    :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+                    :path => "attachments/:id/:style_:basename.:extension"
+
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
-	
-	def url_thumb
-	  @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-	end
+
+  def url_thumb
+    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+  end
 end
