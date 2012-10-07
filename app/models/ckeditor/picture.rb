cfbaef4d443c83eb55cf8d728a229f970ec13447
groteck <groteck@gmail.com>
2012-10-07 21:06:03 +0100

    test s3 conections

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,11 +1,13 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
+                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                    :path => "images/:id/:style_:basename.:extension",
                     :storage => :s3,
-                      :s3_credentials => {
-                      :bucket            => ENV['S3_BUCKET_NAME'],
-                      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                      }, :styles => { :content => '800>', :thumb => '118x100#' }
+                    :s3_credentials => {
+                    :bucket            => ENV['S3_BUCKET_NAME'],
+                    :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                  }, :styles => { :content => '800>', :thumb => '118x100#' }
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
 	validates_attachment_presence :data
