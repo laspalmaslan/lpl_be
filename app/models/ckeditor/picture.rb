c76534d94d9a8aecf68575d2da743b0398b058dc
groteck <groteck@gmail.com>
2012-10-07 21:54:58 +0100

    fixed sintax problem

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -3,7 +3,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :styles => { :content => '800>', :thumb => '118x100#'},
                     :storage => :s3,
                     :s3_credentials => {
-                    :s3_host_name  =>"s3-eu-west-1.amazonaws.com"
+                    :s3_host_name  =>"s3-eu-west-1.amazonaws.com",
                     :bucket            => ENV['S3_BUCKET_NAME'],
                     :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
