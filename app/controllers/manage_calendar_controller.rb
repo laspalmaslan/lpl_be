6989701970a3dc6f74726814b17cea10a22620c7
Fran <fran@fran-K53SJ.(none)>
2012-10-09 20:01:28 +0100

    change things in manage_calendar controller for the events creation

--- app/controllers/manage_calendar_controller.rb
+++ app/controllers/manage_calendar_controller.rb
@@ -5,6 +5,7 @@ class ManageCalendarController < ApplicationController
 
   def new
     @day = Day.new
+    @day.events.build
   end
 
   def edit
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,7 +1,7 @@
 class Day < ActiveRecord::Base
   attr_accessible :day_date, :events_attributes
   has_many :events
-  accepts_nested_attributes_for :events 
+  accepts_nested_attributes_for :events, allow_destroy: true 
 end
 
 
