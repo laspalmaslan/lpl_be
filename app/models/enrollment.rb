fceaeb9f89e22ad1f6bf80b9f3c865a67a425d21
groteck <groteck@gmail.com>
2012-11-18 01:25:02 +0000

    fix association error

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,5 +1,5 @@
 class Enrollment < ActiveRecord::Base
   attr_accessible :birt, :dni_l, :dni_n, :first_name, :hardware, :last_name, :nick, :email
   has_many :participations
-  has_many :enrollments, through: :participations
+  has_many :tournaments, through: :participations
 end
