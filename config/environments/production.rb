3248a620e5438d43071a1fd0bf7809e331a167ee
groteck <groteck@gmail.com>
2012-11-25 22:54:28 +0000

    change port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Lpl::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'mail.ion6.net',
-    :port           => '465',
+    :port           => '26',
     :authentication => :plain,
     :user_name      => ENV['E-USERNAME'],
     :password       => ENV['E-PASSWORD'],
