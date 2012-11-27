4015cf3e64a08c93fbd343f288bb3e07db2417f1
groteck <groteck@gmail.com>
2012-11-27 03:03:18 +0000

    added all user views and routes for enrollments

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Lpl::Application.routes.draw do
   match 'blog/:id/' => "blog#show", :as => :article
   match "competiciones" => "competitions#index", as: :competitions
   match "competiciones/:id" => "competitions#show", as: :competition
+  # user enrollments
   match "inscripciones" => "users_enrollments#index", as: :u_enrollment
+  match "inscripciones/new" => "users_enrollments#new", as: :u_enrollment_new
+  match "inscripciones/create" => "users_enrollments#create", as: :u_enrollment_create, via: :post
+  match "inscripciones/game_new" => "users_enrollments#new_game", as: :u_game_enrollment_new
+  match "inscripciones/game_create" => "users_enrollments#create_game", as: :u_game_enrollment_create, via: :post
 
   match "normativa" => "static_pages#rules", as: :rules
   match "calendar" => "calendar#index", as: :calendar
