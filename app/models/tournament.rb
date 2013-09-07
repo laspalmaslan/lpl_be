d491f3dbfc7b5c6a9c682c994bb593e42462fb1e
groteck <groteck@gmail.com>
2013-09-07 19:50:43 +0100

    add image to tournaments and change style for that #6

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,6 @@
 class Tournament < ActiveRecord::Base
-  attr_accessible :award, :description, :game, :pc
+  attr_accessible :award, :description, :game, :pc, :image
+  has_attached_file :image
   has_many :participations
   has_many :enrollments, through: :participations
 end
