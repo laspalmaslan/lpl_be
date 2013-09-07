bd43b4fdcebed691414b7b93e20baa2ab759d022
groteck <groteck@gmail.com>
2013-09-07 17:57:44 +0100

    change dashboard contet and styles #6

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -4,6 +4,8 @@ def rules
 end
 
 def dashboard
+  @news = Post.where("published_at IS NOT NULL").order("published_at DESC").limit(2)
+  @tournaments = Tournament.order("updated_at DESC").limit(3)
 end
 
 end
